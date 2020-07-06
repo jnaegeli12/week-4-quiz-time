7d48c5b0334138db1b307ea36fc3c4c3d4f1c71c
Judy Naegeli <jnaegeli@gmail.com>
2020-07-05 21:07:21 -0700

    Commit of initial files, unfinished

---
+++ questions.js
@@ -0,0 +1,32 @@
+var questions = [
+    {
+        title: "In an HTML document, in which tag do we put Javascript code?",
+        choices: ["<script>", "<javascript>", "<style>", "<dynamic>"],
+        answer: "<script>"
+    },
+    {
+        title: "Where is the correct place to insert JavaScript in an HTML document?",
+        choices: ["At the top of the <head> section", "Above the <title> in the <head> section", "At the bottom of the <body> section", "At the top of the <body> section"],
+        answer: "At the bottom of the <body> section"
+    },
+    {
+        title: "What is the correct syntax for referring to an external script called 'javascript.js'?",
+        choices: ["<script href='javascript.js'></script>", "<script src='javascript.js'></script>", "<script link='javascript.js'></script>"],
+        answer: "<script src='javascript.js'></script>"
+    },
+    {
+        title: "What is the correct syntax for defining a new function in JavaScript?",
+        choices: ["function.myFunction() { }", "function = myFunction()", "function() { }", "function myFunction() { }"],
+        answer: "function myFunction() { }"
+    },
+    {
+        title: "Which is an example of a string?",
+        choices: ['false', '"New York City"', '8', 'none'],
+        answer: "New York City"
+    },
+    {
+        title: 'How do you display values in the browser console?',
+        choices: ['console.log();', 'console = "";', 'console.log = "";', 'console.display();'],
+        answer: 'console.log();'
+    },
+];
---
+++ sandbox.js
@@ -0,0 +1,15 @@
+// functions to look up: timer,
+
+// Event onclick of a button "start quiz"
+//      A countdown timer begins at the onclick event
+//      Question 1 appears in the browser
+// User answers in the form (CSS) by clicking the cooresponding button
+// Correct answers get a "correct" message and a next button.
+// Wrong answers get a "wrong" message and must choose again until right
+// every time a wrong answer is clicked, :15 seconds are taken off the timer.
+// On question 10, instead of a next button, a "Finish" button comes up.
+// Onclick "finish" button, message "Your score is ...", form to enter initials, two buttons "start over" & "view highscores"
+// Score is time remaining when finished.
+// If all 10 questions have not been answered, then "You score is ..." page appears and score is 0.
+// score is stored in local storage
+// local storage contains the last 10 scores
